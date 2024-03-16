e78a532037159327822b07ee6e5915c07232e1bb
GSP-YT <128293212+GSP-YT@users.noreply.github.com>
2024-03-16 12:56:00 +0530

    Add files via upload

--- mcga.cpp
+++ mcga.cpp
@@ -1,32 +1,22 @@
 #include<stdio.h>
 #include<graphics.h>
-#include<conio.h>
-#include<math.h>
 
-void symmetry(int x,int y, int xc, int yc,int d){
-	putpixel(xc+x,yc-y,GREEN);
-	delay(d);
+void symmetry(int x,int y, int xc, int yc){
+	putpixel(xc+x,yc-y,RED);
 	putpixel(xc+y,yc-x,GREEN);
-	delay(d);
-	putpixel(xc+y,yc+x,GREEN);
-	delay(d);
-	putpixel(xc+x,yc+y,GREEN);
-	delay(d);
+	putpixel(xc+y,yc+x,WHITE);
+	putpixel(xc+x,yc+y,YELLOW);
 	putpixel(xc-x,yc+y,GREEN);
-	delay(d);
-	putpixel(xc-y,yc+x,GREEN);
-	delay(d);
-	putpixel(xc-y,yc-x,GREEN);
-	delay(d);
-	putpixel(xc-x,yc-y,GREEN);
-	delay(d);
+	putpixel(xc-y,yc+x,WHITE);
+	putpixel(xc-y,yc-x,BLUE);
+	putpixel(xc-x,yc-y,YELLOW);
 }
 
-void drawCircle(int xc, int yc, int r,int d){
+void drawCircle(int xc, int yc, int r){
 	int x = 0;
 	int y = r;
 	int p = 1 - r;
-	symmetry(x,y,xc,yc,d);
+	symmetry(x,y,xc,yc);
 	for(x = 0; y > x ; x++){
 		if(p < 0){
 			p += 2 * x + 3;
@@ -34,22 +24,19 @@ void drawCircle(int xc, int yc, int r,int d){
 			p += 2 * (x-y) + 5;
 			y--;
 		}
-		symmetry(x,y,xc,yc,d);
-		delay(d);
+		symmetry(x,y,xc,yc);
 	}
 }
 
 int main(){
-	int x,y,r,d;
+	int x,y,r;
 	printf("Enter center - x, y : ");
 	scanf("%d%d",&x,&y);
 	printf("Enter R : ");
 	scanf("%d",&r);
-	printf("Enter annimation delay : ");
-	scanf("%d",&d);
 	int gd = DETECT , gm;
 	initgraph(&gd,&gm,NULL);
-	drawCircle(x,y,r,d);
+	drawCircle(x,y,r);
 	getch();
 	closegraph();
 	
